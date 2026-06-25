94b600e0dcbeeb430260e7308ea02fae443d2ff3
devalgarg129 <devalgarg978@gmail.com>
2026-06-25 10:51:47 +0530

    Mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const http = require('http');
-
+const { Server } = require('socket.io');
 
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
@@ -73,4 +73,36 @@ function startServer(){
     .connect(mongoURI)
     .then(() => console.log("MONGODB connected successfully....."))
     .catch((err) => console.error("Unable to connect : ", err));
+
+  app.use(cors({ origin: "*" }));
+  app.get("/", (req, res) => {
+    res.send("Welcome");
+  })
+  const user = "test";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ['GET', 'POST']
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", (userID) => {
+      user = userID;
+      console.log("----------");
+      console.log(user);
+      console.log("----------");
+      socket.join(userID);
+    });
+  })
+
+  const db = mongoose.connection;
+  db.once("open", async() => {
+    console.log("CRUD operations called");
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Server is running on the port: ${port}`);
+  })
 }
