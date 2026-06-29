f2df9e3f58623b32bf825dc27b9596f56cdca9ea
devalgarg129 <devalgarg978@gmail.com>
2026-06-29 14:20:00 +0530

    implementing fetch repo

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Dashboard = () => {
+    const [repositories, setRepositories] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
 
+    useEffect(() => {
+        const userId = localStorage.getItem("userId");
+
+        const fetchRepositories = async () => {
+            try{
+                const response = await fetch(`http://localhost:3002/repo/user/${userId}`);
+                const data = await response.json();
+                console.log(data);
+            }catch(error){
+                console.error("Error while fetching the Repository : ", error.message);
+            }
+        }
+
+        const fetchSuggestedRepositories = async () => {
+            try{
+                const response = await fetch("http://localhost:3002/repo/all");
+                const data = await response.json();
+                console.log(data);
+                setSuggestedRepositories(data.repositories);
+            }catch(error){
+                console.error("Error while fetching the Repository : ", error.message);
+            }
+        };
+
+        fetchRepositories();
+    }, []);
 }
 
 export default Dashboard;
