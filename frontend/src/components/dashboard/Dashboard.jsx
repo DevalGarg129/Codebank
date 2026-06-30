e387cc48c02c4cc85aaf6dae8b3a37850f47b1af
devalgarg129 <devalgarg978@gmail.com>
2026-06-30 11:45:57 +0530

    completing dashboard

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -23,15 +23,61 @@ const Dashboard = () => {
             try{
                 const response = await fetch("http://localhost:3002/repo/all");
                 const data = await response.json();
-                console.log(data);
                 setSuggestedRepositories(data.repositories);
+                console.log(suggestedRepositories);
             }catch(error){
                 console.error("Error while fetching the Repository : ", error.message);
             }
         };
 
         fetchRepositories();
+        fetchSuggestedRepositories();
     }, []);
+
+    //Depend on seach query and user repo
+    useEffect(() => {
+        if(searchQuery == ''){
+            setSearchResults(repositories);
+        }else{
+            const filteredRepo = repositories.filter((repo) => 
+                repo.name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setSearchResults(filteredRepo);
+        }
+    }, [searchQuery, repositories]);
+
+    return (
+        <section id="dashboard">
+            <aside>
+                <h3>Suggested Repositories</h3>
+                {suggestedRepositories.map((repo) => {
+                    return(
+                        <div key={repo._id}>
+                        <   h4>{repo.name}</h4>
+                            <h4>{repo.description}</h4>
+                        </div>
+                    );
+                })}
+            </aside>
+            <main>
+                <h2>Your Repositories</h2>
+                {repositories.map((repo) => {
+                    return(
+                        <div key={repo._id}>
+                        <   h4>{repo.name}</h4>
+                            <h4>{repo.description}</h4>
+                        </div>
+                    );
+                })}
+            </main>
+            <aside>
+                <h3>Upcoming Events</h3>
+                <li><p> Tech Conference - 15 December </p></li>
+                <li><p> Developer Conference - 25 December </p></li>
+                <li><p> React Summit - 5 December </p></li>
+            </aside>
+        </section>
+    )
 }
 
 export default Dashboard;
